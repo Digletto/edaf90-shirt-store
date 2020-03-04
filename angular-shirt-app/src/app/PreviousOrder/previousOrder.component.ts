69053eb907e87ce57e43e557ad7b33a719c6b8f3
SofieTroedsson <sofie.troedsson.310@student.lu.se>
2020-03-04 20:00:49 +0100

    fixed routerLink

---
+++ angular-shirt-app/src/app/PreviousOrder/previousOrder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-previousorder',
+    templateUrl: './previousorder.component.html',
+    styleUrls: ['./previousorder.component.css']
+  })
+
+  export class PreviousorderComponent{
+      
+  }
--- angular-shirt-app/src/app/app-routing.module.ts
+++ angular-shirt-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckoutComponent } from './Checkout/checkout.component';
+import { PreviousorderComponent } from './PreviousOrder/previousOrder.component'
+import { Shop } from './Shop/shop.component';
+import { AppComponent } from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'previousorder', component: PreviousorderComponent},
+  {path: 'shop', component: Shop},
+  {path: 'appcomponent', component: AppComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { enableTracing: true })],
