b1db17cb1fb06df596c3b9b0ecf784cb5858b8bd
hannes.berntsson <hannes.berntsson.788@student.lu.se>
2020-03-06 19:57:24 +0100

    added barest framework for checkout page

--- angular-shirt-app/src/app/Checkout/checkout.component.ts
+++ angular-shirt-app/src/app/Checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-checkout',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   })
 
   export class CheckoutComponent{
-      
+
+    constructor(private modalService: NgbModal) {}
+
+    exampleItem : {[key: string]: string} = {
+        color: "red",
+        word: "surreptitious",
+        form: "adjective",
+        def: "kept secret especially because it would not be approved of."
+    }
+
+    /**if (exampleItem.def.length >= 50) {
+    *exampleItem.def = exampleItem.def.substring(0, 50) + '...';
+    *}
+    */  
+
+    shirtFiles : {[key: string]: string} = {
+        black : "../../assets/t-shirt-pictures/black_shirt.jpg",
+        blue : "../../assets/t-shirt-pictures/blue_shirt.jpg",
+        green : "../../assets/t-shirt-pictures/green_shirt.jpg",
+        white : "../../assets/t-shirt-pictures/white_shirt.jpg",
+        yellow : "../../assets/t-shirt-pictures/yellow_shirt.jpg",
+        red : "../../assets/t-shirt-pictures/red_shirt.jpg"       
+    }
+
+    onPayClick() {
+        this.modalService.open("Processing Your Payment...");
+    }
   }
