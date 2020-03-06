14ae57727e351ab881bf8dc7b2b6aa41a73d822c
elt11jos <elt11jos@student.lu.se>
2020-03-06 10:33:33 +0100

    Moved the home page to its own route

--- angular-shirt-app/src/app/app-routing.module.ts
+++ angular-shirt-app/src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Home/home.component';
 import { CheckoutComponent } from './Checkout/checkout.component';
 import { PreviousorderComponent } from './PreviousOrder/previousOrder.component'
 import { Shop } from './Shop/shop.component';
-import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
   {path: 'checkout', component: CheckoutComponent},
+  {path: '', component: HomeComponent},
   {path: 'previousorder', component: PreviousorderComponent},
   {path: 'shop', component: Shop},
-  {path: 'appcomponent', component: AppComponent},
-
 ];
 
 @NgModule({
