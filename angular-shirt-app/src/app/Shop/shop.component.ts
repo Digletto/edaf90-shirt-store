d1380878ed6877bc47e40bfed8dc73e4621a2546
mhall815 <61271498+mhall815@users.noreply.github.com>
2020-03-06 14:52:35 +0100

    added some contet to shop

--- angular-shirt-app/src/app/Shop/shop.component.ts
+++ angular-shirt-app/src/app/Shop/shop.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
+ 
+
 
 @Component({
     selector: 'app-shop',
@@ -6,6 +10,46 @@ import { Component } from '@angular/core';
     styleUrls: ['./shop.component.css']
   })
 
-  export class Shop{
+  export class Shop implements OnInit{
+
+
+    black : String = "../../assets/t-shirt-pictures/black_shirt.jpg";
+    blue : String = "../../assets/t-shirt-pictures/blue_shirt.jpg";
+    green : String = "../../assets/t-shirt-pictures/green_shirt.jpg";
+    white : String = "../../assets/t-shirt-pictures/white_shirt.jpg";
+    yellow : String = "../../assets/t-shirt-pictures/yellow_shirt.jpg";
+    red : String = "../../assets/t-shirt-pictures/red_shirt.jpg";
+    
+    selectedShirt: String = this.white;
+
+
+
+    pic: ImageBitmap;
+
+
+    constructor(){
+
+
+
+    }
+
+    ngOnInit() {
+
+
+
+    }
+
+
+    onChange(event: any) {
+      this.selectedShirt=event.target.value;
+
+
+    }
+
+
+    
+
+
+
       
   }
