6035bdd0d936c9bc8c3c784cbb515988184bc639
hannes.berntsson <hannes.berntsson.788@student.lu.se>
2020-03-10 18:38:05 +0100

    updated checkout and added loop

--- angular-shirt-app/src/app/Checkout/checkout.component.ts
+++ angular-shirt-app/src/app/Checkout/checkout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -7,17 +7,33 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
     styleUrls: ['./checkout.component.css']
   })
 
-  export class CheckoutComponent{
-
-    constructor(private modalService: NgbModal) {}
+  export class CheckoutComponent implements OnInit{
+    
+    constructor(private modalService: NgbModal) {
+        //this.updatePrice();
+    }
 
     exampleItem : {[key: string]: string} = {
         color: "red",
         word: "surreptitious",
         form: "adjective",
-        def: "kept secret especially because it would not be approved of."
+        def: "kept secret especially because it would not be approved of",
+        size: "S",
+        cost: "89"
     }
 
+    exampleItem2 : {[key: string]: string} = {
+        color: "black",
+        word: "verisimilitude",
+        form: "noun",
+        def: "the appearence of being true or real",
+        size: "M",
+        cost: "79"
+    }
+
+    tshirts;
+    total;
+    //total = parseInt(this.exampleItem.cost) + parseInt(this.exampleItem2.cost);
     /**if (exampleItem.def.length >= 50) {
     *exampleItem.def = exampleItem.def.substring(0, 50) + '...';
     *}
@@ -34,5 +50,23 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
     onPayClick() {
         this.modalService.open("Processing Your Payment...");
+    }
+
+    updatePrice() {
+        var shirt;
+        var t = 0;
+        for (shirt of this.tshirts) {
+            t += parseInt(shirt.cost);
+        }
+        this.total = t;
+    }
+
+    ngOnInit() {
+        //this.tshirts = JSON.parse(localStorage.getItem("tshirts"));
+
+        //temp for testing
+        this.tshirts = [this.exampleItem, this.exampleItem2];
+
+        this.updatePrice();
     }
   }
